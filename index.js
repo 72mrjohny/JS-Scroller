f53d822c4ad42546d8f5b054685f0f41956289ad
Jan Pieniążek <johnn93@wp.pl>
2019-08-08 15:52:55 +0200

    Normalize css, styles, basic js

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("Hello World!");
+
+
+
+    const rootElement = document.querySelector('#root');
+    const sections = document.querySelectorAll('section');
+    document.addEventListener('mousewheel', function (event) {
+        console.log(event.wheelDelta)
+    });
+
+})
